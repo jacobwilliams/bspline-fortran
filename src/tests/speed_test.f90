423b7b9481cdf8e00d1b1bef7643ae0328c6511a
Jacob Williams <jacobwilliams@users.noreply.github.com>
2015-12-08 23:14:48 -0600

    added plot to documentation.

--- src/tests/speed_test.f90
+++ src/tests/speed_test.f90
@@ -1,6 +1,9 @@
 !*****************************************************************************************
 !
 !> Speed test for 1d-6d tensor product b-spline interpolation (object-oriented version).
+!
+!# Results
+!  ![Plot of results](https://raw.githubusercontent.com/jacobwilliams/bspline-fortran/master/src/tests/speed_test.png)
 
     program bspline_speed_test
 
