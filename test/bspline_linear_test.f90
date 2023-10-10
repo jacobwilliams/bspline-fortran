2f617bc9e0e671d0a0eac5fd9a268fb524d67176
Jacob Williams <jacobwilliams@users.noreply.github.com>
2023-10-09 21:36:28 -0500

    test updates to avoid aliased arguments to ink routines
    Fixes #89
    also updated the linear test.

--- test/bspline_linear_test.f90
+++ test/bspline_linear_test.f90
@@ -12,28 +12,19 @@
     implicit none
 
     integer(ip),parameter :: nx    = 10    !! number of points in x
-    integer(ip),parameter :: nxv   = 111 !100   !! number of points to evaluate interpolant
-
-    integer(ip),parameter :: kx    = 2    !! order in x [linear]
-    !integer(ip),parameter :: kx    = bspline_order_cubic    !! order in x [cubic]
-    !integer(ip),parameter :: kx    = bspline_order_quartic
+    integer(ip),parameter :: nxv   = 111   !! number of points to evaluate interpolant
     integer(ip),parameter :: iknot = 0    !! automatically select the knots
+    integer,parameter :: idx = 0 !! compute value for the spline interpolation
 
     real(wp) :: x(nx)
-    real(wp) :: xval(nxv),fval(nxv),fval_linear(nxv)
-    real(wp) :: tx(nx+kx)
+    real(wp) :: xval(nxv),fval(nxv),fval_linear(nxv),fval3(nxv),fval4(nxv)
     real(wp) :: fcn_1d(nx)
-    real(wp) :: val,tru,err,errmax
-    integer(ip) :: i,idx,iflag,inbvx,iloy
-    type(pyplot) :: plt
+    real(wp) :: val,err,errmax
+    integer(ip) :: i,iflag,inbvx,iloy
     integer :: istat  !! pyplot-fortran status flag
-    real(wp),dimension(3*kx) :: w1_1d !! work array
+    type(pyplot) :: plt
+    type(bspline_1d) :: b2, b3, b4
     type(linear_interp_1d) :: s1
-
-    idx = 0
-
-    x = huge(1.0_wp)
-    xval = huge(1.0_wp)
 
     do i=1,nx
         x(i) = real(i-1,wp) * 10.0_wp
@@ -47,59 +38,69 @@
     inbvx = 1
     iloy  = 1
 
+    ! the low-level routines are given the wrong result? check this...
+
     ! initialize
-    call db1ink(x,nx,fcn_1d,kx,iknot,tx,fcn_1d,iflag)
-    if (iflag/=0) then
-        write(*,*) 'Error initializing 1D spline: '//get_status_message(iflag)
-    end if
+    call b2%initialize(x,fcn_1d,2,iflag,extrap=.true.) ! linear
+    if (iflag/=0) error stop 'Error initializing 1D linear spline: '//get_status_message(iflag)
+    call b3%initialize(x,fcn_1d,3,iflag,extrap=.true.) ! quadratic
+    if (iflag/=0) error stop 'Error initializing 1D quadratic spline: '//get_status_message(iflag)
+    call b4%initialize(x,fcn_1d,4,iflag,extrap=.true.) ! cubic
+    if (iflag/=0) error stop 'Error initializing 1D cubic spline: '//get_status_message(iflag)
     call s1%initialize(x,fcn_1d,iflag)
-    if (iflag/=0) then
-        write(*,*) 'Error initializing 1D linear interpolator: ', iflag
-    end if
+    if (iflag/=0) error stop 'Error initializing 1D linear interpolator'
 
     !initialize the plot:
     call plt%initialize(grid=.true.,xlabel='x (deg)',ylabel='f(x)',&
-                        title='Linear Test',legend=.true.)
+                        title='Linear Test',legend=.true.,figsize=[10,5])
     call plt%add_plot(x,fcn_1d,&
                         label='Function $f(x) = \sin(x)$',&
                         linestyle='ko',markersize=5,linewidth=2,istat=istat)
 
     errmax = 0.0_wp
     do i=1,nxv
-        call db1val(xval(i),idx,tx,nx,kx,fcn_1d,val,iflag,inbvx,w1_1d,extrap=.true.)
+        call b2%evaluate(xval(i),idx,val,iflag)
         fval(i) = val  ! save it for plot
-        if (iflag/=0) error stop 'error calling db1val: '//get_status_message(iflag)
-
-        tru    = f1(xval(i))
-        err    = abs(tru-val)
-        errmax = max(err,errmax)
-        !write(*,*) xval(i), val, tru, err, iflag
+        if (iflag/=0) error stop 'error evaluating linear spline: '//get_status_message(iflag)
 
         ! also compute linear interpolation:
         call s1%evaluate(xval(i),val)
         fval_linear(i) = val ! linear vector for plot
-        write(*,*) "error : ", xval(i), fval(i) - fval_linear(i)
+
+        err    = abs(fval(i) - fval_linear(i))
+        errmax = max(err,errmax)
+
+        ! also others:
+        call b3%evaluate(xval(i),idx,fval3(i),iflag)
+        if (iflag/=0) error stop 'error evaluating quadratic spline: '//get_status_message(iflag)
+        call b4%evaluate(xval(i),idx,fval4(i),iflag)
+        if (iflag/=0) error stop 'error evaluating cubic spline: '//get_status_message(iflag)
 
     end do
 
-    ! check max error against tolerance
     write(*,*) ''
-    write(*,*) '1D: max error:', errmax
+    write(*,*) 'Max difference (spline - linear):', errmax
     write(*,*) ''
 
-    call plt%add_plot(xval,fval,&
-            label='k=2 Spline Interpolated',&
-            linestyle='g.-',linewidth=1,istat=istat)
     call plt%add_plot(xval,fval_linear,&
             label='Linear Interpolated',&
-            linestyle='r.-',linewidth=1,istat=istat)
+            linestyle='r.',linewidth=1,istat=istat)
+    call plt%add_plot(xval,fval,&
+            label='Linear ($k=2$) Spline Interpolated',&
+            linestyle='r-',linewidth=1,istat=istat)
+    call plt%add_plot(xval,fval3,&
+            label='Quadratic ($k=3$) Spline Interpolated',&
+            linestyle='k.-',linewidth=1,istat=istat)
+    call plt%add_plot(xval,fval4,&
+            label='Cubic ($k=4$) Spline Interpolated',&
+            linestyle='c.-',linewidth=1,istat=istat)
     call plt%savefig('bspline_linear_test.png',istat=istat)
 
     contains
 
         real(wp) function f1(x) !! 1d test function
         implicit none
-        real(wp) :: x
+        real(wp),intent(in) :: x
         real(wp),parameter :: a = acos(-1.0_wp)/18.0_wp
         f1 = sin(a*x)
         end function f1
